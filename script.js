9886fdf68fb96ff1b830c8801e62a05758998fe8
Ubuntu <ubuntu@ip-172-31-36-2.ec2.internal>
2019-08-30 13:41:47 +0000

    fading inout buttons

--- script.js
+++ script.js
@@ -42,6 +42,69 @@ $(document).ready(function() {
         $('#par6').toggle('1000');
     });
 }); 
+
+    $(document).ready(function() {
+    // Create the slideToggle effects each of the paragraphs and
+    // buttons
+    $("#button_effects1").click(function(){
+        $('#par1').slideToggle('1000');
+    });
+    $("#button_effects2").click(function(){
+        $('#par2').slideToggle('1000');
+    });
+    $("#button_effects3").click(function(){
+        $('#par3').slideToggle('1000');
+    });
+    $("#button_effects4").click(function(){
+        $('#par4').slideToggle('1000');
+    });
+    $("#button_effects5").click(function(){
+        $('#par5').slideToggle('1000');
+    });
+    $("#button_effects6").click(function(){
+        $('#par6').slideToggle('1000');
+    });
+});  
+
+// Use the fadeTo effect when the mouse hovers over a specific button
+    // and fadeTo again when the mouse is no longer hovering over the button
+
+    $("#button_effects1").mouseenter(function(){
+        $('#button_effects1').fadeTo(1000, 0.5);
+    });
+    $("#button_effects1").mouseleave(function(){
+        $('#button_effects1').fadeTo(1000, 1);
+    });
+    $("#button_effects2").mouseenter(function(){
+        $('#button_effects2').fadeTo(1000, 0.5);
+    });
+    $("#button_effects2").mouseleave(function(){
+        $('#button_effects2').fadeTo(1000, 1);
+    });
+    $("#button_effects3").mouseenter(function(){
+        $('#button_effects3').fadeTo(1000, 0.5);
+    });
+    $("#button_effects3").mouseleave(function(){
+        $('#button_effects3').fadeTo(1000, 1);
+    });
+    $("#button_effects4").mouseenter(function(){
+        $('#button_effects4').fadeTo(1000, 0.5);
+    });
+    $("#button_effects4").mouseleave(function(){
+        $('#button_effects4').fadeTo(1000, 1);
+    });
+    $("#button_effects5").mouseenter(function(){
+        $('#button_effects5').fadeTo(1000, 0.5);
+    });
+    $("#button_effects5").mouseleave(function(){
+        $('#button_effects5').fadeTo(1000, 1);
+    });
+    $("#button_effects6").mouseenter(function(){
+        $('#button_effects6').fadeTo(1000, 0.5);
+    });
+    $("#button_effects6").mouseleave(function(){
+        $('#button_effects6').fadeTo(1000, 1);
+    });
 //waits until page is loaded first
 
     $(document).ready(function(){
@@ -49,13 +112,14 @@ $(document).ready(function() {
     $("p").click(function(){
         $("p").addClass("highlight_text");
     });
+});    
 
 //will add lightblue to h2 elements
 
     $("h2").hover(function(){
         $("h2").addClass( "h2_color");   
     });
-
+    
     /*
     this will apply larger font size to the active h2 element 
     by adding the h2_font_size class but 
@@ -80,6 +144,7 @@ $(document).ready(function() {
         $("#hr_html").removeClass("h2_font_size");
         $("#hr_css").addClass("h2_font_size");
     });
+    
 
     //applies colour black to body background when mouse enters over buttons
 
@@ -93,4 +158,4 @@ $(document).ready(function() {
     $(".bottom_button").mouseleave(function(){
         $("body").css( "background-color", "#eee"); 
     });
-}); 
+ 
